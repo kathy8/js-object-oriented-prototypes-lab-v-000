64fa8ec8d36780a120c7ee26b7596b6ae0e5634b
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-13 18:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ BoardMember.prototype.doCharity = function(){
 BoardMember.prototype.releasePressStatement = function(){
   return "You will see great things from Scuber."
 }
-//
+
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
 // }
 BoardMember.prototype.sayHi = function(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-	+  }
+	 }
